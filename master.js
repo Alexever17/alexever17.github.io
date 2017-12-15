e5aad57f8302db38c10a89a4f98e3d1d7457b0b7
alexever17@gmail.com <alexever17@gmail.com>
2017-12-15 18:34:39 +0100

    added new js30

--- master.js
+++ master.js
@@ -2,7 +2,7 @@ var database = {
   projects : ["https://alexever17.github.io/NameYoubifier/",
   "https://alexever17.github.io/googlemaps/",
   "https://alexever17.github.io/random-quote-machine/"],
-  js30 : ["https://alexever17.github.io/30daysofjs/001-drumkit/"],
+  js30 : ["https://alexever17.github.io/30daysofjs/001-drumkit/", "https://alexever17.github.io/30daysofjs/002-clock/"],
   css100 : ["https://alexever17.github.io/100DaysOfCss/001-title/",
   "https://alexever17.github.io/100DaysOfCss/002-menu-icon/"]
 };
