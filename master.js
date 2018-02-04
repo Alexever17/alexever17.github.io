bee8cd2fa0e13e21ddb0ee89f8b95b3ad786b512
alexever17@gmail.com <alexever17@gmail.com>
2018-02-04 20:17:47 +0000

    simplified version of the profile page

--- master.js
+++ master.js
@@ -6,7 +6,6 @@
 //   css100 : ["https://alexever17.github.io/100DaysOfCss/001-title/",
 //   "https://alexever17.github.io/100DaysOfCss/002-menu-icon/"]
 // };
-
 function forward(category) {
   alert('Sorry, not enough projects yet');
 }
@@ -18,12 +17,10 @@ function back(category) {
 function mobileMenu() {
     var w = window.innerWidth;
     navbar = document.querySelectorAll(".nav-ul li");
-    console.log(navbar);
     if (w < 500) {
         navbar[1].innerHTML = '<a href="index.html"><img src="icons/003-coding.png" alt="Coding"></a>';
-        navbar[2].innerHTML = '<img src="icons/002-customer-service.png" alt="Business">';
-        navbar[3].innerHTML = '<a href="contact.html"><img src="icons/001-phone.png" alt="Contact"></a>';
-        navbar[4].innerHTML = '<a class="githublink" href="https://github.com/Alexever17/" target="_blank"><img src="icons/002-github.png" alt="Github"></a>';
+        navbar[2].innerHTML = '<a href="contact.html"><img src="icons/001-phone.png" alt="Contact"></a>';
+        navbar[3].innerHTML = '<a class="githublink" href="https://github.com/Alexever17/" target="_blank"><img src="icons/002-github.png" alt="Github"></a>';
     }
 }
 
